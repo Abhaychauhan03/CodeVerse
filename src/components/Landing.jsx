190e1948cb6f399b803ecc836d3e5835406c59d3
Abhay Singh Chauhan <chauhanabhay027@gmail.com>
2023-02-25 11:01:07 +0530

    changes to ui and added session authentication

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,4 +1,4 @@
-import { getDatabase, push, ref, set } from "firebase/database";
+import { getDatabase, onValue, push, ref, set } from "firebase/database";
 import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -22,14 +22,41 @@ function Landing() {
   };
 
   const handleJoinClick = () => {
-    navigate("/codeeditor", {
-      state: {
-        id: sessionid,
+    onValue(
+      ref(db, `sessions/${sessionid}`),
+      (snapshot) => {
+        const sessionData = snapshot.val();
+
+        if (sessionData) {
+          navigate("/codeeditor", {
+            state: {
+              id: sessionid,
+            },
+          });
+        } else {
+          alert("Invalid session ID. Please try again.");
+        }
       },
-    });
+      (error) => {
+        console.error("Error checking session ID:", error);
+      }
+    );
   };
   return (
     <div className="landing">
+      <div class="wave2">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+            class="shape-fill"
+          ></path>
+        </svg>
+      </div>
       <div className="landing__create">
         <button className="create__btn" onClick={handleCreateClick}>
           Create
@@ -46,6 +73,19 @@ function Landing() {
         <button className="join__btn" onClick={handleJoinClick}>
           Join
         </button>
+      </div>
+      <div class="wave">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+            class="shape-fill"
+          ></path>
+        </svg>
       </div>
     </div>
   );
